391e691d5d14918f58d5baed6b27bcb8eca400b6
bayashi <42190+bayashi@users.noreply.github.com>
2025-12-27 03:09:47 +0000

    add a comment to run Fuzz test

--- jl_fuzz_test.go
+++ jl_fuzz_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 )
 
+// go test -fuzz=FuzzJlProcess -fuzztime=5s
 func FuzzJlProcess(f *testing.F) {
 	f.Fuzz(func(t *testing.T, noPrettify bool, showErr bool, splitTab bool, splitLF bool, data []byte) {
 		options := &Options{
